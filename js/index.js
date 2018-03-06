fd5a0a3e6a5b8dfbde3636860ecbada2650cf450
elbert.kim@icloud.com <lkstandsfor@gmail.com>
2018-03-06 11:39:40 -0500

    add quotes

--- js/index.js
+++ js/index.js
@@ -27,6 +27,9 @@ var typed = new Typed('#typed', {
 var quotes = [];
 quotes.push("<em>I check my pulse and if I can find it, I know I've got a chance</em>. <br>-- <em>Paul Newman</em>");
 quotes.push("<em>The only way to be the best is to keep working like you got nothing</em>. <br>-- <em>Mark Wahlberg</em>");
+quotes.push("<em>Every strike brings me closer to the next home run</em>. <br>-- <em>Babe Ruth</em>");
+quotes.push("<em>Expose your practice, don't practice for exposure</em>. <br>-- <em>Ido Portal</em>");
+quotes.push("<em>Anger always backfires, it hurts people, you have to apologize all the time. It's better to skip it</em>. <br>-- <em>Jamie Dimon</em>")
 var quote = document.getElementById("quote");
 quote.innerHTML = quotes[Math.floor(Math.random()*quotes.length)]
 
