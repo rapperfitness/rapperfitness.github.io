c24c68dcef3aaf1147118e968476f07bdb51cbe0
elbert.kim@icloud.com <lkstandsfor@gmail.com>
2018-03-15 15:44:57 -0400

    change typed

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ b.start()
 
 // typed
 var typed = new Typed('#typed', {
-  strings: ["^1000 Hi.", "^1000 I am a Senior at Rutgers University majoring in CS", "^1000 I am graduating in May and currently looking for a full-time job or internship."],
+  strings: ["^1000 Hi, I am a Senior majoring in CS at Rutgers University", "^1000 From 2012 to 2016 I worked as a Production Assistant at a company called Powerhouse. I worked with Korean celebrities and my responsibilties included translating, interacting with media and various other departments, acting as a liasion between Koreans and Americans, and sales."],
   smartBackspace: true,
   backDelay: 100,
   showCursor: false,
@@ -28,7 +28,10 @@ quotes.push("<em>I check my pulse and if I can find it, I know I've got a chance
 quotes.push("<em>The only way to be the best is to keep working like you got nothing</em>. <br>-- <em>Mark Wahlberg</em>");
 quotes.push("<em>Every strike brings me closer to the next home run</em>. <br>-- <em>Babe Ruth</em>");
 quotes.push("<em>Expose your practice, don't practice for exposure</em>. <br>-- <em>Ido Portal</em>");
-quotes.push("<em>Anger always backfires, it hurts people, you have to apologize all the time. It's better to skip it</em>. <br>-- <em>Jamie Dimon</em>")
+quotes.push("<em>Anger always backfires, it hurts people, you have to apologize all the time. It's better to skip it</em>. <br>-- <em>Jamie Dimon</em>");
+quotes.push("<em>If you listen carefully, people explain to you what it is that they need</em>. <br>-- <em>Rick Rubin</em>");
+quotes.push("<em>I never lose. I either win or I learn.</em>. <br>-- <em>Nelson Mandela</em>")
+
 var quote = document.getElementById("quote");
 quote.innerHTML = quotes[Math.floor(Math.random()*quotes.length)]
 
